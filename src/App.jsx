1363c6ba6828be85295204236281ddd9c070d9ab
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-10-14 18:50:23 -0300

    Atividade11

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Texto from './components/Texto'
+
+function App() {
+
+  return (
+    <>
+      <Texto/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Texto.jsx
@@ -0,0 +1,5 @@
+function Texto(){
+    return <h1>Olá, Mundo!</h1>
+}
+
+export default Texto
